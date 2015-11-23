f58b787484183d53a7106c7eb372e349cf14b2d5
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-23 14:21:40 -0800

    Add functions for zooming in on selected locations based on button text.

--- MapMasters/MapViewController.m
+++ MapMasters/MapViewController.m
@@ -13,6 +13,8 @@
 @interface MapViewController ()
 
 @property CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet MKMapView *locationMapView;
+- (IBAction)locationButtonPressed:(id)sender;
 
 @end
 
@@ -21,6 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self requestLocationPermission];
+    [self.locationMapView setShowsUserLocation:YES];
+    self.locationMapView.mapType = MKMapTypeSatellite;
+    self.locationMapView.layer.cornerRadius = 15.0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,4 +37,31 @@
     [self.locationManager requestWhenInUseAuthorization];
 }
 
+- (void)setRegion: (MKCoordinateRegion)region {
+    [self.locationMapView setRegion:region animated:YES];
+}
+
+- (IBAction)locationButtonPressed:(id)sender {
+    if ([sender isKindOfClass:[UIButton class]]) {
+        UIButton *button = sender;
+        NSString *buttonTitle = button.titleLabel.text;
+        if ([buttonTitle isEqualToString:@"Selimiye Mosque"]) {
+            CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(41.678326, 26.559207);
+            MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 600.0, 600.0);
+            [self setRegion:region];
+        }
+        if ([buttonTitle isEqualToString:@"Mehmed Paša Sokolović Bridge"]) {
+            CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(43.782611, 19.287983);
+            MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 600.0, 600.0);
+            [self setRegion:region];
+        }
+        if ([buttonTitle isEqualToString:@"Süleymaniye Mosque"]) {
+            CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(41.016090, 28.964054);
+            MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 600.0, 600.0);
+            [self setRegion:region];
+        }
+    }
+}
 @end
+
+
