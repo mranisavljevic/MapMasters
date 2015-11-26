b33632f770f9bb317534c04070a38792dec806a9
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-25 22:19:48 -0800

    Add login/signup functionality to loginVC.

--- MapMasters/LoginViewController.h
+++ MapMasters/LoginViewController.h
@@ -7,6 +7,7 @@
 //
 
 @import UIKit;
+@import Parse;
 
 typedef void(^LoginViewControllerCompletion)(void);
 
--- MapMasters/LoginViewController.m
+++ MapMasters/LoginViewController.m
@@ -51,17 +51,59 @@
     self.signupButton.layer.borderWidth = 1.0;
     self.signupButton.layer.borderColor = [darkBrownColor CGColor];
     self.signupButton.layer.cornerRadius = 15.0;
-    self.signupButton.alpha = 0.0;
     self.emailTextField.alpha = 0.0;
 }
 
 - (IBAction)loginButtonPressed:(UIButton *)sender {
-    [UIView animateWithDuration:0.4 animations:^{
-        self.emailTextField.alpha = (self.emailTextField.alpha == 0.0 ? 1.0 : 0.0);
-        self.signupButton.alpha = (self.emailTextField.alpha == 0.0 ? 1.0 : 0.0);
-    }];
+    if ([self.loginButton.titleLabel.text isEqualToString:@"Log In"]) {
+        if (![self.usernameTextField.text isEqualToString:@""]) {
+            if (![self.passwordTextField.text isEqualToString:@""]) {
+                [PFUser logInWithUsernameInBackground:self.usernameTextField.text password:self.passwordTextField.text block:^(PFUser * _Nullable user, NSError * _Nullable error) {
+                    if (error) {
+                        NSLog(@"%@", error.userInfo);
+                    }
+                    if (user) {
+                        if (self.completion) {
+                            self.completion();
+                        }
+                    }
+                }];
+            }
+        } else {
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Oops!" message:@"Please enter a valid username and password." preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+            [alertController addAction:okAction];
+            [self presentViewController:alertController animated:YES completion:nil];
+        }
+    } else {
+        if (![self.usernameTextField.text isEqualToString:@""]) {
+            if (![self.passwordTextField.text isEqualToString:@""]) {
+                if (![self.emailTextField.text isEqualToString:@""]) {
+                    PFUser *newUser = [PFUser user];
+                    newUser.username = self.usernameTextField.text;
+                    newUser.password = self.passwordTextField.text;
+                    newUser.email = self.emailTextField.text;
+                    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+                        if (error) {
+                            NSLog(@"%@", error.userInfo);
+                        }
+                        if (succeeded) {
+                            if (self.completion) {
+                                self.completion();
+                            }
+                        }
+                    }];
+                }
+            }
+        }
+    }
 }
 - (IBAction)signupButtonPressed:(UIButton *)sender {
+    [UIView animateWithDuration:0.4 animations:^{
+        self.emailTextField.alpha = (self.emailTextField.alpha == 0.0 ? 1.0 : 0.0);
+    }];
+    self.signupButton.alpha = (self.emailTextField.alpha == 0.0 ? 1.0 : 0.0);
+    self.loginButton.titleLabel.text = ([self.loginButton.titleLabel.text isEqualToString:@"Log In"] ? @"Sign Up" : @"Log In");
 }
 
 #pragma mark - UITextFieldDelegate
