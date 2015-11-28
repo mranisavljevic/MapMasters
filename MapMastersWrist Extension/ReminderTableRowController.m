53bce36d8abb6db96532276432de32594dda2160
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-28 13:29:45 -0800

    Add watch row controller file and initial layout.

---
+++ MapMastersWrist Extension/ReminderTableRowController.m
@@ -0,0 +1,13 @@
+//
+//  ReminderTableRowController.m
+//  MapMasters
+//
+//  Created by Miles Ranisavljevic on 11/28/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import "ReminderTableRowController.h"
+
+@implementation ReminderTableRowController
+
+@end
